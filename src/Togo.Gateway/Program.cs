6fb66c0524f64683bd9981ca74087356591b7602
Ashwath Mabiyan <ashwathmabiyan@gmail.com>
2026-01-25 16:06:35 +0530

    Minor corrections

--- src/Togo.Gateway/Program.cs
+++ src/Togo.Gateway/Program.cs
@@ -1,10 +1,12 @@
 using Togo.Gateway.Policies;
+using Yarp.ReverseProxy.LoadBalancing;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<ILoadBalancingPolicy, ConsistentHashLoadBalancingPolicy>();
+
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
-    .AddLoadBalancingPolicy<ConsistentHashLoadBalancingPolicy>();
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 app.UseForwardedHeaders();

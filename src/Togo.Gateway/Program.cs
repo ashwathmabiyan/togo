97fab6eab6a7df06753a64c965cf6df5d0eed8e1
Ashwath Mabiyan <ashwathmabiyan@gmail.com>
2026-01-25 16:06:37 +0530

    feature: Dynamic Configuration Polling - Added custom configprovider and maintained fallback as existing configloader

--- src/Togo.Gateway/Program.cs
+++ src/Togo.Gateway/Program.cs
@@ -1,12 +1,28 @@
+using Togo.Gateway.Configuration;
 using Togo.Gateway.Policies;
+using Togo.Gateway.Services;
+using Yarp.ReverseProxy.Configuration;
 using Yarp.ReverseProxy.LoadBalancing;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Register Core Services
 builder.Services.AddSingleton<ILoadBalancingPolicy, ConsistentHashLoadBalancingPolicy>();
 
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+// Register Dynamic Configuration Services
+builder.Services.AddHttpClient<RemoteConfigClient>();
+
+// Initial Fallback Config
+var initialRoutes = new List<RouteConfig>();
+var initialClusters = new List<ClusterConfig>();
+var configProvider = new Togo.Gateway.Configuration.InMemoryConfigProvider(initialRoutes, initialClusters);
+
+builder.Services.AddSingleton(configProvider);
+builder.Services.AddSingleton<IProxyConfigProvider>(configProvider);
+builder.Services.AddHostedService<ConfigPollingWorker>();
+
+builder.Services.AddReverseProxy(); 
+// Note: We removed .LoadFromConfig() because we are providing our own IProxyConfigProvider
 
 var app = builder.Build();
 app.UseForwardedHeaders();
